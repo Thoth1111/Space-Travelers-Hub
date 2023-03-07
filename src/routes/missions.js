48f5bc250c1912ebf045e3262efbd971e8bdaecf
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-03-07 17:15:55 +0300

    Render list of missions in mission route

--- src/routes/missions.js
+++ src/routes/missions.js
@@ -1,16 +1,30 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const Missions = () => (
-  <table>
-    <thead>
-      <tr>
-        <th>Mission</th>
-        <th>Description</th>
-        <th>Status</th>
-      </tr>
-    </thead>
-    <tbody />
-  </table>
-);
+const Missions = () => {
+    const missionList = useSelector((state) => state.missions);
+
+    return (
+        <table>
+            <thead>
+            <tr>
+                <th>Mission</th>
+                <th>Description</th>
+                <th>Status</th>
+            </tr>
+            </thead>
+            <tbody>
+                {missionList.map((mission) => (
+                    <tr key={mission.mission_id}>
+                       <td>{mission.mission_name}</td>
+                       <td>{mission.description}</td>
+                       <td>Conditional component</td>
+                       <td>Conditional component</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    )
+};
 
 export default Missions;
