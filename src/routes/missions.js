40c20e7de54a42b960b003865999951ca4e5619a
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-03-07 12:02:43 +0300

    Add view component for missions

--- src/routes/missions.js
+++ src/routes/missions.js
@@ -3,6 +3,14 @@ import React from "react";
 export const missions = () => {
     return (
         <table>
+            <thead>
+                <tr>
+                    <th>Mission</th>
+                    <th>Description</th>
+                    <th>Status</th>
+                    <th></th>
+                </tr>
+            </thead>
             <tbody>
                 
             </tbody>
