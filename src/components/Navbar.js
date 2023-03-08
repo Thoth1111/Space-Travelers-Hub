bd5d4d0576d6aa18596f81b7007c732aee68bc9b
amklass11 <lassanaa14@gmail.com>
2023-03-08 06:27:09 +0100

    empty navbar setup with image

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+const Navbar = () => {
+    return (
+      <div className="navbar-logo">
+        <div className="logo">
+            <img className="imglogo" src="https://cdn-icons-png.flaticon.com/512/3212/3212567.png" alt="logo" />
+        </div>
+        <ul className="navbar-links">
+          <li>Rockets</li>
+          <li>Misiions</li>
+          <li>My Profile</li>
+        </ul>
+      </div>
+    );
+  };
+  export default Navbar;
+  
