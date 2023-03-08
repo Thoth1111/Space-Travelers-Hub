eda07cc698dbd9584b1a60a30a9bf36bd126392c
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-03-08 10:58:37 +0300

    Fix Linters

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,21 @@
 import { NavLink } from 'react-router-dom';
 import '../styles/nav.css';
 
-const Navbar = () => {
-    return (
-      <div className="navbar-logo">
-        <div className="logo">
-            <img className="imglogo" src="https://cdn-icons-png.flaticon.com/512/3212/3212567.png" alt="logo" />
-        </div>
-        <ul className="navbar-links">
-          <li>Rockets</li>
-          <li>
-             <NavLink to="/missions" activeClassName="active">Missions</NavLink>
-          </li>
-          <li>
-             <NavLink to="/profile" activeClassName="active">Profile</NavLink>
-          </li>
-        </ul>
-      </div>
-      );
-    };
+const Navbar = () => (
+  <div className="navbar-logo">
+    <div className="logo">
+      <img className="imglogo" src="https://cdn-icons-png.flaticon.com/512/3212/3212567.png" alt="logo" />
+    </div>
+    <ul className="navbar-links">
+      <li>Rockets</li>
+      <li>
+        <NavLink to="/missions" activeClassName="active">Missions</NavLink>
+      </li>
+      <li>
+        <NavLink to="/profile" activeClassName="active">Profile</NavLink>
+      </li>
+    </ul>
+  </div>
+);
 
 export default Navbar;
